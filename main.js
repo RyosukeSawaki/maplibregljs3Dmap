053fb0078cdda2b338f55d54f8f13b2329108625
佐脇凌輔 <aa122086@gs.gsc.aoyama.ac.jp>
2025-11-17 01:36:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const map = new maplibregl.Map({
   container: "map",
-  center: [139.7024, 35.6598], // 中心座標
-  zoom: 16, // ズームレベル
+  center: [139.243641, 35.624908], // 中心座標
+  zoom: 10, // ズームレベル
   style: {
     // スタイル仕様のバージョン番号。8を指定する
     version: 8,
